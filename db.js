7bba5c63126985fe1e16d89eafb9fe9d4f32bf2f
mwilson35 <mwilson151288@hotmail.com>
2025-05-13 00:13:43 +0200

    Fix database connection to explicitly use environment variables

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-console.log('db.js loaded'); // Debugging log
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', 
-    password: '', 
-    database: 'belfast_rides'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        process.exit(1);
-    }
-    console.log('Connected to the MySQL database!');
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    process.exit(1);
+  }
+  console.log('Connected to the MySQL database!');
 });
 
 module.exports = connection;
