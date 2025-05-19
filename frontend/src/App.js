bcdd03803431419d7b4ed2f5cf6525e355f6231b
mwilson35 <159795825+mwilson35@users.noreply.github.com>
2025-05-19 16:34:05 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Signup from './pages/Signup'; // New Signup Page
+import Signup from './pages/Signup'; 
 import RiderDashboard from './pages/RiderDashboard';
 import DriverDashboard from './pages/DriverDashboard';
 import AdminDashboard from './pages/AdminDashboard';
