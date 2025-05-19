35c54c784a5d9dd21bcc6462f34a729eb93b4356
mwilson35 <159795825+mwilson35@users.noreply.github.com>
2025-05-19 15:50:58 +0200

    Update db.js

--- db.js
+++ db.js
@@ -13,7 +13,7 @@ connection.connect((err) => {
     console.error('Error connecting to the database:', err);
     process.exit(1);
   }
-  console.log('Connected to the MySQL database!');
+ 
 });
 
 module.exports = connection;
